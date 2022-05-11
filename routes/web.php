5662fbf09887063ddf4e201286867a772c7abecc
arnaudaploon <arnaudadjovi274@gmail.com>
2022-05-11 09:32:04 +0100

    web php modif

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes hi
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
