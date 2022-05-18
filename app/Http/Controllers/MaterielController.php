e8f294048200b0a97a35d42b809026bebf6b999a
arnaudaploon <arnaudadjovi274@gmail.com>
2022-05-18 16:58:17 +0100

    Modification de la base de donnée

--- app/Http/Controllers/MaterielController.php
+++ app/Http/Controllers/MaterielController.php
@@ -8,6 +8,7 @@ use App\Models\Materiel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 
 class MaterielController extends Controller
 {
