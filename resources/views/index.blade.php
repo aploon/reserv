67b370f28f331483e4a8e4d307d7e9866e1c4b6e
arnaudaploon <arnaudadjovi274@gmail.com>
2022-05-18 20:29:31 +0100

    Modification du loader

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -57,6 +57,56 @@
     </div>
 @endsection
 
+@section('preloader')
+    <div class="" id="splash"
+    style="display: flex; align-item: center; position:fixed; top:0; left:0; width:100%; height:100%; background:linear-gradient(45deg,#222428,#444856);color:#fff;z-index:4000;">
+    {{-- <div class="w-100 text-center text-light">
+        <style>
+            @keyframes r {
+                0% {
+                    transform: rotate(0deg);
+                }
+
+                100% {
+                    transform: rotate(45deg);
+                }
+            }
+
+            #splash {
+                transition: all .3s linear
+            }
+
+            #splash.hidden {
+                opacity: 0;
+                z-index: -1;
+                visibility: hidden;
+            }
+
+            svg.gear {
+                animation: r .5s infinite linear;
+            }
+
+        </style><svg class="gear" style="width:64px" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 100 100">
+            <path id="p" fill="#fdb"
+                d="M94.1 58.8c.6-2.8.9-5.8.9-8.8s-.3-6-.9-8.8l-11.7-.4c-.7-2.6-1.7-5-3-7.3l8-8.5c-3.3-4.9-7.5-9.2-12.5-12.5l-8.5 8c-2.3-1.3-4.7-2.3-7.3-3l-.3-11.6C56 5.3 53 5 50 5s-6 .3-8.8.9l-.4 11.7c-2.6.7-5 1.7-7.3 3l-8.5-8c-4.9 3.3-9.2 7.5-12.5 12.5l8 8.5c-1.3 2.3-2.3 4.7-3 7.3l-11.6.3C5.3 44 5 47 5 50s.3 6 .9 8.8l11.7.4c.7 2.6 1.7 5 3 7.3l-8 8.5c3.3 4.9 7.5 9.2 12.5 12.5l8.5-8c2.3 1.3 4.7 2.3 7.3 3l.4 11.7c2.7.5 5.7.8 8.7.8s6-.3 8.8-.9l.4-11.7c2.6-.7 5-1.7 7.3-3l8.5 8c4.9-3.3 9.2-7.5 12.5-12.5l-8-8.5c1.3-2.3 2.3-4.7 3-7.3l11.6-.3zM50 66.9c-9.3 0-16.9-7.6-16.9-16.9S40.7 33.1 50 33.1 66.9 40.7 66.9 50 59.3 66.9 50 66.9z">
+            </path>
+        </svg><svg class="gear" style="width:64px;margin:64px 0 0 -12px;animation-direction:reverse"
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+            <use href="#p"></use>
+        </svg>
+        <h1 class="font-weight-bold text-shadow" style="font-family:Trebuchet MS,Helvetica,sans-serif">Loading.io
+        </h1>
+        <div class="text-muted">loading.io is now loading ...</div>
+    </div> --}}
+    <div class="m-auto w-100 text-center">
+        <img class="" src="{{ asset("template/assets/img/preloader/eclipse_preloader_white.gif") }}" alt="">
+        <div class="" style="font-family:Trebuchet MS,Helvetica,sans-serif; font-size: 30px; font-weight: bold;" backgound="{{ asset("template/assets/img/preloader/eclipse_preloader_white.gif") }}">Reserv</div>
+    </div>
+
+    </div>
+@endsection
+
 @section('main-content')
     <div class="container">
 
@@ -200,6 +250,7 @@
     <script>
         jQuery(document).ready(function() {
 
+            $("#splash").addClass("hidden");
 
             $.ajax({
                 url: "reservations/fullcalendar",
